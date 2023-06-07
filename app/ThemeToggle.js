5910a70369bf19b6c29999d035d0ccda24cd0b2a
adamrodrigues11 <adamrodrigues11@gmail.com>
2023-06-07 00:50:47 -0700

    made changes so that the system theme setting overrides local storage saved theme settings

--- app/ThemeToggle.js
+++ app/ThemeToggle.js
@@ -5,11 +5,17 @@ import { TbMoon, TbSun, TbCircle } from 'react-icons/tb'
 
 const ThemeToggle = () => {
   const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
+  const { theme, systemTheme, setTheme } = useTheme();
 
   useEffect(() => {
     setMounted(true);
   }, []);
+
+  // listen for system theme changes, and override theme value saved in local storage (since I give priority to system theme)
+  // eventually we can update this when next-themes supports system theme detection in the useTheme hook
+  useEffect(() => {
+    setTheme(systemTheme);
+  }, [systemTheme, setTheme]);
 
   if (!mounted) {
     return (
