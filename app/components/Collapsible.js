b748a22529454b8e8df77b700a8c179406be39cb
adamrodrigues11 <adamrodrigues11@gmail.com>
2023-06-10 15:58:33 -0700

    finished project cards

--- app/components/Collapsible.js
+++ app/components/Collapsible.js
@@ -6,7 +6,7 @@ import { useState, useRef, useEffect } from "react";
 export default function Collapsible({
   title,
   children,
-  startOpen = true,
+  startOpen = false,
   containerClasses = "w-full mx-auto py-2 rounded-sm flex flex-col",
 }) {
   const [isOpen, setIsOpen] = useState(startOpen);
@@ -33,13 +33,13 @@ export default function Collapsible({
 
   return (
     <div className={containerClasses}>
-      <div className="flex flex-row justify-start gap-2 items-center cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+      <div className="flex flex-row w-fit justify-start gap-2 items-center cursor-pointer group" onClick={() => setIsOpen(!isOpen)}>
         <h3 className="font-bold">{title}</h3>
         <div>
             {isOpen ? (
-                <FaChevronCircleUp className="text-2xl text-pink-600" />
+                <FaChevronCircleUp className="text-2xl text-pink-600 group-hover:animate-bounce" />
                 ) : (
-                <FaChevronCircleDown className="text-2xl text-pink-600" />
+                <FaChevronCircleDown className="text-2xl text-pink-600 group-hover:animate-bounce" />
                 )
             }  
         </div>
--- app/components/Project.js
+++ app/components/Project.js
@@ -3,28 +3,36 @@ import Image from 'next/image'
 
 export default function Project({ title, category, thumbnail, technologies, description, demo, github }) {
     return (
-        <div className='flex flex-col items-start py-6 max-w-project-card'>
+        <div className='h-fit flex flex-col items-start p-2 gap-2 max-w-project-card bg-slate-200 dark:bg-slate-700 rounded-md shadow-md'>
             <h3 className='text-xl font-bold'>{title}</h3>
-            <h4 className='text-sm sm:text-base text-slate-500'>{category}</h4>
-            <div className='bg-slate-200 dark:bg-slate-700 rounded-md shadow-md w-full aspect-square relative z-0'>
-                {thumbnail && <Image src={thumbnail} alt={title} fill={true} className='rounded-md p-4 absolute z-10'/>}
+            <h4 className='text-sm sm:text-base'>{category}</h4>
+            <div className='w-full aspect-square rounded-md relative z-0 overflow-hidden group'>
+                {thumbnail && <Image src={thumbnail} alt={title} fill={true} className='absolute z-10'/>}
+                <div className='flex gap-12 justify-center items-center absolute top-0 left-0 w-full h-full z-20'>
+                    {demo && 
+                        <button className='bg-slate-700 p-4 rounded-lg shadow-md hover:scale-105 opacity-0 group-hover:opacity-90 transition-opacity ease-linear'>
+                            <a href={demo} target='_blank' rel='noreferrer' className='text-lg text-slate-50'>Demo</a>
+                        </button>
+                    }
+                    {github &&
+                        <button className='bg-slate-700 p-4 rounded-lg shadow-md hover:scale-105 opacity-0 group-hover:opacity-90 transition-opacity ease-linear'>
+                            <a href={github} target='_blank' rel='noreferrer' className='text-lg text-slate-50'>Github</a>
+                        </button> 
+                    }
+                </div>
             </div>
             {technologies && technologies.length > 0 && 
                 <div className='flex flex-row flex-wrap items-center gap-2'>
                     {technologies.map((tech, index) => (
-                        <div key={index} className='text-sm border border-slate-500 rounded-md p-1'>{tech}</div>
+                        <div key={index} className='text-sm border border-slate-500 rounded-md p-0.5'>{tech}</div>
                     ))}
                 </div>
             }
             {description && ( 
-                <Collapsible title='Description'>
+                <Collapsible title='Details'>
                     {description}
                 </Collapsible>
             )}
-            <div className='flex flex-row gap-4'>
-                {demo && <a href={demo} target='_blank' rel='noreferrer' className='text-sm sm:text-base text-slate-500 hover:text-slate-400'>Demo</a>}
-                {github && <a href={github} target='_blank' rel='noreferrer' className='text-sm sm:text-base text-slate-500 hover:text-slate-400'>Github</a>}
-            </div>
         </div>
     )
 }
