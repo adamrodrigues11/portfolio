9ddba27ccbb436e26f1247de1557976792fb2110
adamrodrigues11 <adamrodrigues11@gmail.com>
2023-06-10 23:36:36 -0700

    layout of the details page is finished. just adding data now

--- app/components/ProjectCard.js
+++ app/components/ProjectCard.js
@@ -9,7 +9,7 @@ export default function ProjectCard({ slug, title, category, thumbnail, technolo
                 <h3 className='text-xl font-bold'>{title}</h3>
                 <h4 className='text-sm sm:text-base'>{category}</h4>
                 <div className='w-full aspect-square rounded-md relative z-0 overflow-hidden'>
-                    {thumbnail && <Image src={thumbnail} alt={title} fill={true} className='absolute z-10 group-hover:blur-xs'/>}
+                    {thumbnail && <Image src={thumbnail.path} alt={thumbnail.alt} fill={true} className='absolute z-10 group-hover:blur-xs'/>}
                     <div className='flex gap-12 justify-center items-center absolute top-0 left-0 w-full h-full z-20'>
                         <div className='text-xl bg-slate-200 dark:bg-slate-700 py-4 px-8 rounded-lg shadow-md opacity-0 group-hover:opacity-90 transition-opacity ease-linear'>
                             <p className='border-b border-transparent hover:border-pink-500'>Details</p>
--- app/projects/[slug]/page.js
+++ app/projects/[slug]/page.js
@@ -6,18 +6,19 @@ export default function ProjectDetails({ params }) {
     return(
         <main>
             <h2 className="text-3xl text-center font-bold">{project.title}</h2>
-            <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2-1 lg:grid-cols-3-1 gap-4">
+            <h3 className="text-lg text-center">{project.category}</h3>
+            <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2-1 lg:grid-cols-3-1 gap-8">
                 <div className="sm:col-span-2 md:col-span-1 flex flex-col items-start gap-2">
-                    <h3 className="text-xl font-bold">{project.category}</h3>
+                    <h4 className="text-lg font-bold">About the Application</h4>
                     <div className="flex flex-row flex-wrap items-center gap-2">
                         {project.technologies.map((tech, index) => (
                             <div key={index} className="text-sm border border-slate-500 rounded-md p-0.5">{tech}</div>
                         ))}
                     </div>
-                    <p className='text-sm sm:text-base'>{project.details}</p>
+                    <p className='text-sm sm:text-base'>{project.appDetails}</p>
                 </div>
-                <div className="max-w-[300px] place-self-center w-full sm:max-w-[500px] aspect-square rounded-md relative z-0 overflow-hidden">
-                    {project.thumbnail && <Image src={project.thumbnail} alt={project.title} fill={true} className="absolute z-10"/>}
+                <div className="max-w-[400px] place-self-center w-full sm:max-w-[500px] aspect-square rounded-md relative z-0 overflow-hidden">
+                    {project.thumbnail && <Image src={project.thumbnail.path} alt={project.thumbnail.alt} fill={true} className="absolute z-10"/>}
                 </div>
                 <div className='flex flex-row sm:flex-col justify-center place-items-center gap-6 md:flex-row md:justify-start md:col-span-2'>
                     {project.links?.github && <button className='text-lg bg-slate-200 dark:bg-slate-700 py-4 px-8 rounded-lg shadow-md hover:scale-102 group'>
@@ -26,6 +27,15 @@ export default function ProjectDetails({ params }) {
                     {project.links?.demo && <button className='text-lg bg-slate-200 dark:bg-slate-700 py-4 px-8 rounded-lg shadow-md hover:scale-102 group'>
                         <a className='border-b border-transparent group-hover:border-pink-500' href={project.links.demo} target='_blank' rel='noreferrer'>Demo</a>
                     </button>}
+                </div>
+            </div>
+            <div className="p-4 grid grid-cols-1 lg:grid-cols-2 gap-8">
+                <div className="lg:order-2 flex flex-col items-start gap-2">
+                    <h4 className="text-lg font-bold">My Role</h4>
+                    <p className='text-sm sm:text-base'>{project.role}</p>
+                </div>
+                <div className="max-w-[400px] place-self-center w-full lg:order-1 sm:max-w-[600px] aspect-square rounded-md relative z-0 overflow-hidden">
+                    {project.image && <Image src={project.image.path} alt={project.image.alt} fill={true} style={{objectFit: "contain"}} className="absolute z-10"/>}
                 </div>
             </div>
         </main>
