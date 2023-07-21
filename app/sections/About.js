9e82ebc13a2c58053265695867fdb901dcfcf7cb
adamrodrigues11 <adamrodrigues11@gmail.com>
2023-07-20 17:01:54 -0700

    updated about me

--- app/sections/About.js
+++ app/sections/About.js
@@ -5,7 +5,7 @@ export default function About() {
     return (
         <section id="about" className="pt-24">
             <h2>About Me</h2>
-            <p>Hi, I&apos;m Adam, a junior full stack developer with a passion for engineering creative solutions for complex problems. Having recently completed the Software Systems Developer program at BCIT, I&apos;m currently looking for a full-time position as a junior developer. My technical expertise is in designing fully responsive and intuitive user interfaces, along with programming robust and logical backend code. I also have hands-on experience working with cloud services and serverless architectures.</p>
+            <p>Hi, I&apos;m Adam, a junior full stack developer with a passion for engineering creative solutions to complex problems. Having recently completed the Software Systems Developer program at BCIT, I&apos;m currently looking for a full-time position as a junior developer. My technical expertise is in designing fully responsive and intuitive user interfaces, along with programming robust and logical backend code. I also have hands-on experience working with cloud services and serverless architectures.</p>
             <Collapsible title='My Story'>
                 <p>Mathematical physics &mdash;&gt; teaching &mdash;&gt; programming.</p>
                 <p>My diverse academic and professional background has equipped me with the skills and experience to approach novel problems with unique insights. After completing a BSc. in Mathematical Physics at the University of Waterloo, I pursued a profession in teaching. In the past 5 years, I earned a BEd. from UBC and have helped hundreds of students master concepts in physics, mathematics, and chemistry.</p>
